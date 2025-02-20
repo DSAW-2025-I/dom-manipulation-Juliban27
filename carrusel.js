5e4f9b63061daecc8804bb15ff287a3c13d18921
Julian Romero <Juliban27@gmail.com>
2025-02-19 20:03:14 -0500

    creacion carrusel

--- carrusel.js
+++ carrusel.js
@@ -1,6 +1,39 @@
 let index = 0;
-let carrusel = document.querySelector('.carrusel-image');
+let images = document.querySelectorAll('.carrusel-image');
+let btnPrev = document.querySelector(".btn-prev");
+let btnNext = document.querySelector(".btn-next");
 
-const showImage = (index) => {
+
+const showImage = (indexS) => {
     images.forEach(img => img.classList.remove('active'));
+    images[indexS].classList.add('active');
 }
+const updateButtons = () => {
+    if (index === 0) {
+        btnPrev.style.visibility = "hidden";
+    } else {
+        btnPrev.style.visibility = "visible"; 
+    }
+
+    if (index === images.length - 1) {
+        btnNext.style.visibility = "hidden";
+    } else {
+        btnNext.style.visibility = "visible";
+    }
+};
+
+const prevSlide = () => {
+    index = (index - 1) % images.length;
+    showImage(index);
+    updateButtons();
+
+};
+
+const nextSlide = () => {
+    index = (index + 1) % images.length;
+    showImage(index);
+    updateButtons(); 
+
+};
+
+updateButtons();
